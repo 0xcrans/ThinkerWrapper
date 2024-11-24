7da779d267a395b7418d1bf991a510fa294dfafb
Crans <108697172+0xcrans@users.noreply.github.com>
2024-11-24 15:10:28 +0100

    Update and rename App.js to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from 'react';
+import CransActivityViewer from './pages/CransActivityViewer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CransActivityViewer />
+    </div>
+  );
+}
+
+export default App;
